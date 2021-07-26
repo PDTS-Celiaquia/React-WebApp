119a1b0e510368320a201ff785483c6324dbbbc1
Leopoldo Lening Celaya <57147656+pololeningcelaya@users.noreply.github.com>
2021-07-26 16:35:10 -0300

    hehe

---
+++ src/components/auth/ChangePasswordPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChangePasswordPage() {
+    return (
+        <div>
+             <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Cambia tu contraseña acá</a>
+        </div>
+    )
+}
