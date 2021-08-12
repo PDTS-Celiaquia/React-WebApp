d304feedacc3b8a3c794b1328be7b9e1641ee4bb
TobiasDemoor <tobias.demoor@hotmail.com>
2021-08-12 12:40:30 -0300

    primitive grid

--- src/components/alimentos/AlimentoForm.js
+++ src/components/alimentos/AlimentoForm.js
@@ -1,6 +1,6 @@
-import { Button, Checkbox, Container, IconButton, Typography, withStyles } from '@material-ui/core';
-import { ArrowBack } from '@material-ui/icons';
 import React, { Component } from 'react'
+import { Button, Checkbox, Container, IconButton, Typography, withStyles, FormControlLabel, Grid } from '@material-ui/core';
+import { ArrowBack } from '@material-ui/icons';
 import { connect } from 'react-redux';
 import { findById } from '../../helpers/findById';
 import { findAlimentoById } from '../../services/alimento.service';
@@ -84,13 +84,69 @@ class AlimentoForm extends Component {
                     </Typography>
                 </div>
                 {loading ? <Loader /> :
-                    <>
-                        <p>{JSON.stringify(alimento)}</p>
-                        <Checkbox checked={alimento.esAccesible} onChange={this.handleAccesibleChange} />
+                    <Grid container spacing={1}>
+                        <Grid item xs={6}>
+                            <p>nombre</p>
+                            <p>clasif</p>
+                            <p>genero_especie_variedad</p>
+                            <p>tipo</p>
+                            <p>energia_kJ</p>
+                            <p>agua</p>
+                            <p>proteina</p>
+                            <p>grasa_total</p>
+                            <p>carbohidrato_total</p>
+                            <p>carbohidrato_disponible</p>
+                            <p>fibra_dietetica</p>
+                            <p>ceniza</p>
+                            <p>sodio</p>
+                            <p>potasio</p>
+                            <p>calcio</p>
+                            <p>fosforo</p>
+                            <p>hierro</p>
+                            <p>zinc</p>
+                            <p>tiamina</p>
+                            <p>rivoflavina</p>
+                            <p>niacina</p>
+                            <p>vitamina_c</p>
+                            <p>acidos_grasos_saturados</p>
+                            <p>acidos_grasos_monoinsaturados</p>
+                            <p>acidos_grasos_poliinsaturados</p>
+                            <p>colesterol</p>
+                            <p>Es accesible</p>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <p>{alimento.nombre}</p>
+                            <p>{alimento.clasif}</p>
+                            <p>{alimento.genero_especie_variedad}</p>
+                            <p>{alimento.tipo}</p>
+                            <p>{alimento.energia_kJ}</p>
+                            <p>{alimento.agua}</p>
+                            <p>{alimento.proteina}</p>
+                            <p>{alimento.grasa_total}</p>
+                            <p>{alimento.carbohidrato_total}</p>
+                            <p>{alimento.carbohidrato_disponible}</p>
+                            <p>{alimento.fibra_dietetica}</p>
+                            <p>{alimento.ceniza}</p>
+                            <p>{alimento.sodio}</p>
+                            <p>{alimento.potasio}</p>
+                            <p>{alimento.calcio}</p>
+                            <p>{alimento.fosforo}</p>
+                            <p>{alimento.hierro}</p>
+                            <p>{alimento.zinc}</p>
+                            <p>{alimento.tiamina}</p>
+                            <p>{alimento.rivoflavina}</p>
+                            <p>{alimento.niacina}</p>
+                            <p>{alimento.vitamina_c}</p>
+                            <p>{alimento.acidos_grasos_saturados}</p>
+                            <p>{alimento.acidos_grasos_monoinsaturados}</p>
+                            <p>{alimento.acidos_grasos_poliinsaturados}</p>
+                            <p>{alimento.colesterol}</p>
+                            <Checkbox checked={alimento.esAccesible} onChange={this.handleAccesibleChange} />
+                        </Grid>
                         <Button color="primary" variant="contained" onClick={this.saveChanges}>
                             GUARDAR CAMBIOS
                         </Button>
-                    </>
+                    </Grid>
                 }
             </Container>
         )
